829fb879b8920fa33eeece0739fc3c5c44ecf750
Nikki Sengar <nikkisengar@gmail.com>
2018-12-29 19:03:10 +0530

    First Commit

---
+++ Declarations and Access Modifiers/src/com/j2se/date/DateClassTest.java
@@ -0,0 +1,15 @@
+package com.j2se.date;
+
+import java.util.Date;
+//import java.sql.Date;
+
+public class DateClassTest {
+
+	public static void main(String[] args) {
+		Date d=new Date();
+		
+		System.out.println(d.getYear());
+
+	}
+
+}
