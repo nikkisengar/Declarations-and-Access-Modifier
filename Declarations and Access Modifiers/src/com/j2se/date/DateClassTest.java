397710b319fe7fd76ead786ab953ec01444795e6
Nikki Sengar <nikkisengar@gmail.com>
2018-12-29 19:06:34 +0530

    Second commit

--- Declarations and Access Modifiers/src/com/j2se/date/DateClassTest.java
+++ Declarations and Access Modifiers/src/com/j2se/date/DateClassTest.java
@@ -1,7 +1,7 @@
 package com.j2se.date;
 
 import java.util.Date;
-//import java.sql.Date;
+//import java.sql.Date; //Using this statement also will through ambiguous error 
 
 public class DateClassTest {
 
